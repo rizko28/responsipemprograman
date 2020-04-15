1bfc23f143fb354055f1d18a5a07b3453e4e185d
MUHAMMAD RIZKO MUNZAL <61963883+rizko28@users.noreply.github.com>
2020-04-15 12:30:04 +0700

    Add files via upload

---
+++ program (2).cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2847
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan(190302124, "joni", 2000000);
+
+
+            Console.WriteLine("NIK \t Nama \t\t Gaji");
+            Console.WriteLine("-------------------------------");
+            karyawan.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine(" Asyiiiiik kenaikan gaji 10!!");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("-------------------------------");
+            karyawan.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int gajiBulanan { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if (gaji < 0)
+            {
+                this.gajiBulanan = 0;
+            }
+            else
+            {
+                this.gajiBulanan = gaji;
+            }
+        }
+
+        public void showKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * gajiBulanan;
+            gajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+    }
+}
